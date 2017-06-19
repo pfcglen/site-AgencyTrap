227e33103c750b9810f144505d1bc5bb05286a60
Glen Mark Cabiladas <gcabiladas@gmail.com>
2017-06-19 14:45:44 +0800

    Stress upload
    Stress upload

---
+++ js/custom-scroll.js
@@ -0,0 +1,71 @@
+$(document).ready(function () {
+    //code from css-tricks
+    // Select all links with hashes
+    $('a[href*="#"]')
+        // Remove links that don't actually link to anything
+        .not('[href="#"]')
+        .not('[href="#0"]')
+        .click(function (event) {
+            // On-page links
+            if (
+                location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') &&
+                location.hostname == this.hostname
+            ) {
+                // Figure out element to scroll to
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                // Does a scroll target exist?
+                if (target.length) {
+                    // Only prevent default if animation is actually gonna happen
+                    event.preventDefault();
+                    $('html, body').animate({
+                        scrollTop: target.offset().top
+                    }, 1000, function () {
+                        // Callback after animation
+                        // Must change focus!
+                        var $target = $(target);
+                        $target.focus();
+                        if ($target.is(":focus")) { // Checking if the target was focused
+                            return false;
+                        } else {
+                            $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
+                            $target.focus(); // Set focus again
+                        };
+                    });
+                }
+            }
+        });
+
+    //menu click and scroll event
+    // ------------------------------
+    // https://twitter.com/mattsince87
+    // ------------------------------
+
+    function scrollNav() {
+        $('.mainmenu nav a').click(function () {
+            //Toggle Class
+            $(".active").removeClass("active");
+            $(this).closest('li').addClass("active");
+            var theClass = $(this).attr("class");
+            $('.' + theClass).parent('li').addClass('active');
+            //Animate
+            $('html, body').stop().animate({
+                scrollTop: $($(this).attr('href')).offset().top
+            }, 400);
+            return false;
+        });
+
+    }
+
+    scrollNav();
+    //menu click and scroll event
+
+
+    //back to top
+    $('#scrollToTp').click(function () {
+        $("html, body").animate({
+            scrollTop: 0
+        }, 600);
+        return false;
+    });
+}); //end of ready
---
+++ js/custom-testimonial.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+
+    //testimonial 
+    $('#jQuery-cycle').cycle({
+        fx: 'fade',
+        timeout: 4000000000000000000000000000000,
+        speed:10000000000000000000000000,
+        fit: 0,
+        pause: true,
+        pager:  '#cyleDots'
+    });
+}); //end of ready
+
+/*
+
+$(document).ready(function () {
+
+    //testimonial 
+    $('#jQuery-cycle').cycle({
+        fx: 'fade',
+        timeout: 4000,
+        speed:100,
+        fit: 0,
+        pause: true,
+        pager:  '#cyleDots'
+    });
+}); //end of ready
+*/
